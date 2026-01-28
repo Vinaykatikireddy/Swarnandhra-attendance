c4215de8a0d4b356dd954aa24dde8110c0ac5473
Vinay Katikireddy <vinaykatikireddy@hotmail.com>
2026-01-28 21:05:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,5 +99,5 @@ def api_feedback():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, threaded=True)
+    app.run(host="0.0.0.0", port=5000, threaded=True)
+
